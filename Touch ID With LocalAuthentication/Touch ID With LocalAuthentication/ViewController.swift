62b53732b175dec4ffd104cf3fac9a282c896059
Samman Bikram Thapa <sthapa@dynamicsignal.com>
2018-09-20 12:15:22 -0700

    Add Touch ID with LocalAuthentication

---
+++ Touch ID With LocalAuthentication/Touch ID With LocalAuthentication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Touch ID With LocalAuthentication
+//
+//  Created by Samman Thapa on 9/20/18.
+//  Copyright © 2018 Samman Thapa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
