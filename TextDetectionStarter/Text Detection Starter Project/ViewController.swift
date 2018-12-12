46703f206e4ca7ffb5be27e935cce8dd3de6f269
Samman Bikram Thapa <sthapa@dynamicsignal.com>
2018-12-12 11:43:35 -0800

    Adds drawing character boxes + handles the VNRequests

--- TextDetectionStarter/Text Detection Starter Project/ViewController.swift
+++ TextDetectionStarter/Text Detection Starter Project/ViewController.swift
@@ -61,7 +61,7 @@ class ViewController: UIViewController
     }
 }
 
-// For Vision requests
+// MARK: - Vision requests
 extension ViewController
 {
     func startTextDetection()
@@ -81,10 +81,119 @@ extension ViewController
         }
         
         let result = observations.map({$0 as? VNTextObservation}) // transform all observations in to type VNTextObservation
+        
+        DispatchQueue.main.async {
+            // remove the bottommost layers
+            self.imageView.layer.sublayers?.removeSubrange(1...)
+            
+            for region in result
+            {
+                guard let rg = region else
+                { continue }
+                
+                self.highlightWord(box: rg)
+                
+                if let boxes = region?.characterBoxes
+                {
+                    for characterBox in boxes
+                    {
+                        self.highlightLetters(box: characterBox)
+                    }
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Draw boxes
+extension ViewController
+{
+    func highlightWord(box: VNTextObservation)
+    {
+        // All boxes that were recognized as text boxes
+        guard let boxes = box.characterBoxes else
+        {
+            return
+        }
+        
+        var maxX: CGFloat = 9999.0
+        var minX: CGFloat = 0.0
+        var maxY: CGFloat = 9999.0
+        var minY: CGFloat = 0.0
+        
+        for char in boxes
+        {
+            if char.bottomLeft.x < maxX
+            {
+                maxX = char.bottomLeft.x
+            }
+            if char.bottomRight.x > minX
+            {
+                minX = char.bottomRight.x
+            }
+            if char.bottomRight.y < maxY
+            {
+                maxY = char.bottomRight.y
+            }
+            if char.topRight.y > minY
+            {
+                minY = char.topRight.y
+            }
+        }
+        
+        let xCord = maxX * imageView.frame.size.width
+        let yCord = (1 - minY) * imageView.frame.size.height
+        let width = (minX - maxX) * imageView.frame.size.width
+        let height = (minY - maxY) * imageView.frame.size.height
+        
+        // Draw a box around each word
+        let outline = CALayer()
+        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
+        outline.borderWidth = 2.0
+        outline.borderColor = UIColor.red.cgColor
+        
+        imageView.layer.addSublayer(outline)
+    }
+    
+    func highlightLetters(box: VNRectangleObservation)
+    {
+        let xCord = box.topLeft.x * imageView.frame.size.width
+        let yCord = (1 - box.topLeft.y) * imageView.frame.size.height
+        let width = (box.topRight.x - box.bottomLeft.x) * imageView.frame.size.width
+        let height = (box.topLeft.y - box.bottomLeft.y) * imageView.frame.size.height
+        
+        // Draw a box around each letter
+        let outline = CALayer()
+        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
+        outline.borderWidth = 1.0
+        outline.borderColor = UIColor.blue.cgColor
+        
+        imageView.layer.addSublayer(outline)
     }
 }
 
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate
 {
-    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection)
+    {
+        // check if sampleBuffer exists and is giving us AVCaptureOutput
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else
+        { return }
+        
+        // VNImageOption type can hold properites and data from camera
+        var requestOptions:[VNImageOption : Any] = [:]
+        
+        if let camData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
+            requestOptions = [.cameraIntrinsics:camData]
+        }
+        
+        // On getting all the requests, handle the requests
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation(rawValue: 6)!, options: requestOptions)
+        
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
 }
