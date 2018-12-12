a54b0cc4e0a3c8f56cddc2d9584ff34e3bfe985b
Samman Bikram Thapa <sthapa@dynamicsignal.com>
2018-12-12 11:18:51 -0800

    Adds a session of live streaming video onto the screen
    from the phones default mode of getting image (camera)

--- TextDetectionStarter/Text Detection Starter Project/ViewController.swift
+++ TextDetectionStarter/Text Detection Starter Project/ViewController.swift
@@ -14,20 +14,49 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    override func viewDidLoad() {
+    // Used whenever you want to perform some actions based on a live stream
+    var session = AVCaptureSession()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        
+    override func viewWillAppear(_ animated: Bool)
+    {
+        startLiveVideo()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews()
+    {
+        imageView.layer.sublayers?[0].frame = imageView.bounds
+    }
+    
+    func startLiveVideo()
+    {
+        // 1 Create a session to capture video at high quality
+        session.sessionPreset = AVCaptureSession.Preset.photo
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) // gets us livestream
+        
+        // 2 set the input and output of the session
+        let deviceInput = try! AVCaptureDeviceInput(device: captureDevice!) // The default camera
+        let deviceOutput = AVCaptureVideoDataOutput() // what the video should appear as
+        deviceOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA)] // video format
+        deviceOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.default))
+        
+        session.addInput(deviceInput)
+        session.addOutput(deviceOutput)
+        
+        // 3 Add the video capturing to screen
+        let imageLayer = AVCaptureVideoPreviewLayer(session: session)
+        imageLayer.frame = imageView.bounds
+        imageView.layer.addSublayer(imageLayer)
+        
+        session.startRunning()
     }
 }
 
-extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate
+{
     
 }
