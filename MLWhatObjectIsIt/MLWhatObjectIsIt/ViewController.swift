be150f10e3d13324225b17dcc28a6fbc30e71670
Samman Bikram Thapa <sthapa@dynamicsignal.com>
2018-12-12 15:30:07 -0800

    Start ML project and add all UI components

---
+++ MLWhatObjectIsIt/MLWhatObjectIsIt/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MLWhatObjectIsIt
+//
+//  Created by Samman Thapa on 12/12/18.
+//  Copyright © 2018 Samman Thapa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
