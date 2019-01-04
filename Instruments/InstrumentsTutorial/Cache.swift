c7b79ee0b98394e00edc62a9e9124b064033e9c5
Samman Bikram Thapa <sthapa@dynamicsignal.com>
2019-01-04 08:02:37 +0545

    removes all cached images when os broadcasts a low memory warning

--- Instruments/InstrumentsTutorial/Cache.swift
+++ Instruments/InstrumentsTutorial/Cache.swift
@@ -31,40 +31,51 @@
 import UIKit
 
 class ImageCache {
-  
-  private static let _shared = ImageCache()
-  
-  var images = [String:UIImage]()
-  
-  static var shared: ImageCache {
-    return _shared
-  }
-  
-  func loadThumbnail(for photo: FlickrPhoto, completion: @escaping FlickrAPI.FetchImageCompletion) {
-    // load from cache, if exists
-    if let image = ImageCache.shared.image(forKey: photo.id) {
-        completion(Result.success(image))
+    
+    private static let _shared = ImageCache()
+    
+    var images = [String:UIImage]()
+    
+    static var shared: ImageCache {
+        return _shared
     }
-    else {
-        FlickrAPI.loadImage(for: photo, withSize: "m") { result in
-            // save to cache
-            if case .success(let image) = result {
-                ImageCache.shared.set(image, forKey: photo.id)
-            }
-            completion(result)
+    
+    init()
+    {
+        NotificationCenter.default.addObserver(forName: Notification.Name.UIApplicationDidReceiveMemoryWarning, object: nil, queue: .main) { [weak self] notification in
+            self?.images.removeAll(keepingCapacity: false)
         }
     }
-  }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func loadThumbnail(for photo: FlickrPhoto, completion: @escaping FlickrAPI.FetchImageCompletion) {
+        // load from cache, if exists
+        if let image = ImageCache.shared.image(forKey: photo.id) {
+            completion(Result.success(image))
+        }
+        else {
+            FlickrAPI.loadImage(for: photo, withSize: "m") { result in
+                // save to cache
+                if case .success(let image) = result {
+                    ImageCache.shared.set(image, forKey: photo.id)
+                }
+                completion(result)
+            }
+        }
+    }
 }
 
 // MARK: - Custom Accessors
 extension ImageCache {
-  
-  func set(_ image: UIImage, forKey key: String) {
-    images[key] = image
-  }
-  
-  func image(forKey key: String) -> UIImage? {
-    return images[key]
-  }
+    
+    func set(_ image: UIImage, forKey key: String) {
+        images[key] = image
+    }
+    
+    func image(forKey key: String) -> UIImage? {
+        return images[key]
+    }
 }
