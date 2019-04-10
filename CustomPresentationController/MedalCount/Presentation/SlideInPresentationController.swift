64f1de932dc4b9ca37a69fe5ecb76871debe4593
Samman Bikram Thapa <sthapa@dynamicsignal.com>
2019-04-10 06:20:18 -0700

    Implement dimming of dimming view with transition during transition will begin

--- CustomPresentationController/MedalCount/Presentation/SlideInPresentationController.swift
+++ CustomPresentationController/MedalCount/Presentation/SlideInPresentationController.swift
@@ -8,6 +8,17 @@
 
 import UIKit
 
+/**
+ Before you can start customizing the transition, you have to override four methods and a property
+ from UIPresentationController. The default methods don’t do anything, so there’s no need to call
+ super
+ 
+ The four methods are;
+ - presentationTransitionWillBegin()
+ - dismissalTransitionWillBegin()
+ - containerViewWillLayoutSubviews()
+ - size(forChildContentContainer:withParentContainerSize:) -> CGSize
+ */
 class SlideInPresentationController: UIPresentationController
 {
   // 4.0: Setting up the dimming view
@@ -31,6 +42,35 @@ class SlideInPresentationController: UIPresentationController
     setupDimmingView()
   }
 
+  // 5.0
+  // First, for a smooth transition, override presentationTransitionWillBegin() to make the dimming
+  // view fade in along with the presentation.
+  override func presentationTransitionWillBegin()
+  {
+    // 5.0.1
+    containerView?.insertSubview(dimmingView, at: 0)
+    
+    // 5.0.2: Check this out to know more about visual format:
+    // https://developer.apple.com/library/archive/documentation/UserExperience/Conceptual/AutolayoutPG/VisualFormatLanguage.html
+    NSLayoutConstraint.activate(
+      NSLayoutConstraint.constraints(withVisualFormat: "V:|[dimmingView]|",
+                                     options: [], metrics: nil, views: ["dimmingView": dimmingView]))
+    NSLayoutConstraint.activate(
+      NSLayoutConstraint.constraints(withVisualFormat: "H:|[dimmingView]|",
+                                     options: [], metrics: nil, views: ["dimmingView": dimmingView]))
+    
+    // 5.0.3 UIPresentationController‘s transitionCoordinator has a very cool method to animate
+    // things during the transition. In this section, you set the dimming view’s alpha property to
+    // 1.0 along the presentation transition.
+    guard let coordinator = presentedViewController.transitionCoordinator else {
+      dimmingView.alpha = 1.0
+      return
+    }
+    
+    coordinator.animate(alongsideTransition: { _ in
+      self.dimmingView.alpha = 1.0
+    })
+  }
 }
 
 // MARK: - Private
